6b9a0e427688d25823928223c3ea47260457b162
Aaron Wilson <codingsensei19@gmail.com>
2020-02-02 22:58:32 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,98 +1,37 @@
+
+def sort_array_asc(integers)
+  integers.sort
+end 
+
+def sort_array_desc(integers)
+  integers.sort{|a,b| b<=>a}
+end
+
+def sort_array_char_count(strings)
+  strings.sort{|a,b| a.length<=>b.length}
+end
+
 def swap_elements(array)
-  array[1..2] = array[1..2].sort do |a,b|
-    if a==b || a<b || a>b
-      1
-    end
-  end
-  array
+  array.sort{|a,b| a[1]<=>b[2]}
 end
 
-test_array = [1,2,3,4]
-string_test = ["blake", "ashley", "scott"]
-#swap_elements(test_array)
-
-def swap_elements_from_to(array, index, new_index)
-  array[index], array[new_index] = array[new_index], array[index]
-  return array
+def reverse_array(array)
+  array.reverse
 end
-
-describe 'swap_elements_from_to' do
-  it 'swaps elements and allows you to specify the index of the element you would like to move to a new index' do
-    expect(swap_elements_from_to(["one", "two", "three"], 2, 1)).to eq(["one", "three", "two"])
-  end
-end
-
-############################################
-
-
-
-
-
 
 def kesha_maker(array)
-  return_array = []
-  array.each do |string|
-    string[3] = "$"
-    return_array << string
-  end
-  return_array
+  array.collect {|arr| arr.delete(arr[2]).insert(2,"$")}
 end
 
 def find_a(array)
-  array.select do |string|
-    string.start_with?("a")
-  end
+  array.keep_if {|arr| arr[0]=="a"}
 end
-
-#a_array = ["apple","bake","ceaser","avocado"]
-#find_a(a_array)
 
 def sum_array(array)
-  #Advanced: Try using the .inject method here.
-  #(5..10).inject {|sum, n| sum + n }
-  sum = 0
-  array. each do |number|
-    if number.is_a? Integer
-      sum += number
-    end
-  end
-  sum
+  #same as array.inject(0){|sum,arr| sum+arr}
+  array.inject(0, :+)
 end
-
-def sum_array_inject(array)
-    array.inject do |sum,number|
-        sum + number
-    end
-end
-
-number_array =[0,1.07,2,3,4,5.2,6]
-#sum_array(number_array)
-
-#puts number_array.inject {|sum, n| sum + n}
-#sum_array_inject(number_array)
 
 def add_s(array)
-  #Build a method that adds an "s" to each word in the array except for the second element in the array ("feet" is already plural).
-  return_array = []
-      array.each_with_index do |word,index|
-        if index != 1
-          word = word + "s"
-        end
-        return_array << word
-  end
-
-  return_array
+  array.collect{|arr| arr==array[1] ? arr:arr <<"s"}
 end
-
-def add_s_collect(array)
-      array.each_with_index.collect do |word,index|
-        if index != 1
-          word = word + "s"
-        else
-          word = word
-        end
-  end
-end
-
-#s_word_array = ["hand","feet", "knee", "table"]
-#add_s_collect(s_word_array)
